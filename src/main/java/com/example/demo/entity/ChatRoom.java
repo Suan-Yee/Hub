3cd936ca6229466937ce7a335f3d84b5fa07f994
jijii368 <naychioo.slt1234@gmail.com>
2024-05-02 11:19:27 +0630

    template added

--- src/main/java/com/example/demo/entity/ChatRoom.java
+++ src/main/java/com/example/demo/entity/ChatRoom.java
@@ -1,31 +1,30 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import java.util.Date;
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "chat_room")
 @Entity
+@Builder
 public class ChatRoom {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @ManyToOne
-    private User sender;
-    @ManyToOne
-    private User recipient;
-    private String chatRoomId;
+    private int id;
 
-    @PrePersist
-    public void onPrePersist() {
-        chatRoomId = UUID.randomUUID().toString();
-    }
+    private String chatId;
+
+    private int recipientId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
