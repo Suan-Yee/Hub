779cb81617204c31618b4af7834efd62218ce72f
jijii368 <naychioo.slt1234@gmail.com>
2024-05-02 11:14:10 +0630

    template added

--- src/main/java/com/example/demo/services/impl/ChatRoomServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/ChatRoomServiceImpl.java
@@ -6,9 +6,12 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.ChatRoomRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.services.ChatRoomService;
+import com.example.demo.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -17,26 +20,81 @@ public class ChatRoomServiceImpl implements ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
     private final UserRepository userRepository;
+    private UserService userService;
+
+//    @Override
+//    public ChatRoom checkOrCreateChatRoom(Long senderId,Long recipientId) {
+//       Optional<ChatRoom> chatRoom = chatRoomRepository.findBySenderAndRecipientId(senderId,recipientId);
+//
+//       if(chatRoom.isPresent()) {
+//           return chatRoom.get();
+//       }
+//        User sender = userRepository.findById(senderId).orElse(null);
+//        User recipient = userRepository.findById(recipientId).orElse(null);
+//
+//        ChatRoom newChat = new ChatRoom();
+//        newChat.setSender(sender);
+//        newChat.setRecipient(recipient);
+//
+//        return chatRoomRepository.save(newChat);
+//    }
+//    @Override
+//    public ChatRoom findByChatRoomId(String chatRoomId){
+//        ChatRoom chatRoom = chatRoomRepository.findByChatRoomId(chatRoomId).orElse(null);
+//        return chatRoom;
+//    }
+
+//    @Transactional
+//    @Override
+//    public ChatRoom save(String name) {
+//        ChatRoom chatRoom = ChatRoom.builder()
+//                .date(new Date())
+//                .name(name)
+//                .build();
+//        return chatRoomRepository.save(chatRoom);
+//    }
+
+//    @Override
+//    public ChatRoom findById(Long id) {
+//        return chatRoomRepository.findById(id).orElseThrow();
+//    }
 
     @Override
-    public ChatRoom checkOrCreateChatRoom(Long senderId,Long recipientId) {
-       Optional<ChatRoom> chatRoom = chatRoomRepository.findBySenderAndRecipientId(senderId,recipientId);
+    public Optional<String> getChatRoomId(User senderId, Long recipientId, boolean createNewRoomIfNotExists) {
+        if (senderId == null || recipientId == 0) {
+            throw new IllegalArgumentException("Sender ID and Recipient ID must not be null or empty.");
+        }
 
-       if(chatRoom.isPresent()) {
-           return chatRoom.get();
-       }
-        User sender = userRepository.findById(senderId).orElse(null);
-        User recipient = userRepository.findById(recipientId).orElse(null);
+        Optional<ChatRoom> existingRoom = chatRoomRepository.findByUserAndRecipientId(senderId, Math.toIntExact(recipientId));
+        if (existingRoom.isPresent()) {
+            return Optional.of(existingRoom.get().getChatId());
+        }
 
-        ChatRoom newChat = new ChatRoom();
-        newChat.setSender(sender);
-        newChat.setRecipient(recipient);
+        if (createNewRoomIfNotExists) {
+            String chatId = createChatId(senderId.getId(),recipientId);
+            return Optional.of(chatId);
+        }
 
-        return chatRoomRepository.save(newChat);
+        return Optional.empty();
     }
-    @Override
-    public ChatRoom findByChatRoomId(String chatRoomId){
-        ChatRoom chatRoom = chatRoomRepository.findByChatRoomId(chatRoomId).orElse(null);
-        return chatRoom;
+
+    private String createChatId(Long senderId, Long recipientId) {
+        String chatId = String.format("%s_%s", senderId, recipientId);
+        ChatRoom senderRecipient = new ChatRoom();
+        User user=userService.findById(senderId);
+        senderRecipient.setChatId(chatId);
+        senderRecipient.setUser(user);
+        senderRecipient.setRecipientId(Math.toIntExact(recipientId));
+
+        ChatRoom recipientSender = new ChatRoom();
+        recipientSender.setChatId(chatId);
+        User user1=userService.findById(recipientId);
+        recipientSender.setUser(user1);
+        recipientSender.setRecipientId(Math.toIntExact(senderId));
+
+        chatRoomRepository.save(senderRecipient);
+        chatRoomRepository.save(recipientSender);
+
+        return chatId;
     }
 }
