fdb122cd6dbc123becaa70f48a8ca79f0d5bb49c
htetpyaeyoon <htetpyaeyoon2003@gmail.com>
2024-05-28 16:17:13 +0630

    guideline added

---
+++ src/main/java/com/example/demo/repository/GuideLinesResposity.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+import com.example.demo.entity.GuideLines;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuideLinesResposity extends JpaRepository<GuideLines,Long> {
+}
